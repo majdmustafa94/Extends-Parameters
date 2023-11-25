9b724fc856502d0603b27cdd072d49506e036e68
Raghadmustafa96 <majdmustafa1994@gmail.com>
2023-11-25 16:08:46 +0300

    update

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,7 +3,11 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MyTest extends Parameters {
 
@@ -28,8 +32,33 @@ public class MyTest extends Parameters {
         Mysoftassert.assertEquals(image_check3,false,"image3 with image1");
 
         Mysoftassert.assertAll();
+    }
 
+    @Test()
+    public void validate_the_contact() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
+        String Email[]={"m1@gmail.com","m2gmail.com","m3@gmail","m4@gmail.jo"};
+        Random Email_index= new Random();
+        int index= Email_index.nextInt(4);
+
+        driver.findElement(By.xpath("//*[@id=\"navbarExample\"]/ul/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"recipient-email\"]")).sendKeys(Email[index]);
+        Thread.sleep(1000);
+
+        String myvalue= driver.findElement(By.xpath("//*[@id=\"recipient-email\"]")).getAttribute("value");
+        System.out.println(myvalue);
+
+        String regex= "^[A-Za-z0-9+_.-]+@(.+)+\\.(.+)$";
+        Pattern p= Pattern.compile(regex);
+        Matcher m= p.matcher(Email[index]);
+
+        System.out.println(Email[index]+" is valid email : "+m.matches());
+
+        boolean input_email= m.matches();
+
+        Mysoftassert.assertEquals(input_email,true);
+        Mysoftassert.assertAll();
     }
 
 }
